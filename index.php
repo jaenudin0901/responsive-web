9259647608082c943d4e000d44006e66853ebe29
jaenudin0901 <jaenudinj913198@gmail.com>
2020-07-25 21:39:39 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Complete Responsive webiste Design</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="banner">
+        <header>
+            <a href="#" class="logo">Jaenudin</a>
+            <div class="toggle"></div>
+        </header>
+        <div class="content">
+            <img src="assets/bg.jpg" alt="">
+            <h2>Success By Design</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem commodi voluptatem deleniti accusantium dolores? Deleniti, accusantium amet? Dolores maiores nostrum veniam esse earum dolore dolorum architecto quaerat? Laudantium tenetur voluptatem soluta atque doloremque blanditiis dolor, nam harum illo molestias incidunt!</p>
+        </div>
+    </div>
+    <!-- about -->
+    <section class="about">
+        <div class="contentBx">
+            <h2 class="heading">About Us</h2>
+            <p class="text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum, quam eveniet cum laboriosam ipsa eaque sit aliquam assumenda, nulla voluptate tempora ipsum fugit. Sed accusamus, quis neque repellat tenetur iure ut provident, officiis quia ab magni atque! Consequuntur nam enim voluptatem veritatis atque praesentium tempora ea,<br><br>
+                eos alias, aperiam repellat facilis cum. Odio omnis ratione eligendi. Nulla ea corporis aspernatur facere sint animi accusantium minus. Nisi rerum dicta quaerat ex adipisci repellendus. Ea voluptatem porro facilis a voluptas aliquam culpa aut quasi! Sunt officia neque distinctio qui? Officia harum ullam molestiae exercitationem ipsum ab maiores tenetur! Nobis explicabo vel unde?</p>
+        </div>
+        <div class="imgBx"></div>
+    </section>
+    <!-- services -->
+    <section class="services">
+        <h2 class="heading">Our Services</h2>
+        <p class="text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur, incidunt ex, itaque maxime quasi eligendi, autem blanditiis minima optio explicabo esse doloribus. Expedita temporibus asperiores rem. Expedita aperiam amet, perferendis ab soluta cumque obcaecati quo eligendi enim beatae eius id error, quibusdam aut excepturi ipsum vel sint optio temporibus? Assumenda?</p>
+        <div class="container">
+            <div class="serviceBx">
+                <div>
+                    <img src="assets/icon1.png" alt="">
+                    <h2>Design</h2>
+                </div>
+            </div>
+            <div class="serviceBx">
+                <div>
+                    <img src="assets/icon2.png" alt="">
+                    <h2>Development</h2>
+                </div>
+            </div>
+            <div class="serviceBx">
+                <div>
+                    <img src="assets/icon3.png" alt="">
+                    <h2>Web progrming</h2>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- texhnology -->
+    <div class="technology">
+        <div class="contentBx">
+            <h2 class="heading">We use cutting edge technology</h2>
+            <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, dicta quae obcaecati minus cumque eius voluptate consectetur hic dolores quo eum temporibus blanditiis cum quidem consequuntur explicabo dolor, fugiat iure incidunt, facilis ab porro! Voluptates voluptatum et nesciunt atque totam fugiat non incidunt illum quisquam dignissimos? Earum et non amet?</p>
+        </div>
+        <div class="imgBx">
+            <img src="assets/tech.png" alt="">
+        </div>
+    </div>
+
+    <!-- Client -->
+    <section class="client">
+        <h2 class="heading">Our Client</h2>
+        <p class="text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam ad doloremque voluptatem, laborum repudiandae maiores iure suscipit itaque unde voluptate, minima odit ducimus? Sapiente necessitatibus ut sint vitae obcaecati at, perferendis vel tempore similique inventore exercitationem quae rerum totam eveniet non temporibus unde optio dolor fugit iste veniam modi laudantium!</p>
+        <div class="imgBx">
+            <img src="assets/brand1.png" alt="">
+            <img src="assets/brand2.png" alt="">
+            <img src="assets/brand3.png" alt="">
+            <img src="assets/brand4.png" alt="">
+        </div>
+    </section>
+    <!-- testimonials -->
+    <section class="testimonials">
+        <h2 class="heading">What our Client says</h2>
+        <div class="container">
+            <div class="contentBx">
+                <div>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure ut corrupti magni incidunt illo tenetur, laborum et facilis repellendus ab minus, ex, deleniti voluptates possimus assumenda numquam delectus? Natus corporis architecto, consequatur quos maxime, alias dolore illo neque optio quo atque nulla laborum aliquid? Ipsum non dolores aliquam ratione molestiae fugiat magni voluptates! Ab possimus a, fuga quam hic excepturi!</p><br>
+                    <h3>Someone Famous</h3>
+                </div>
+            </div>
+            <div class="contentBx">
+                <div>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure ut corrupti magni incidunt illo tenetur, laborum et facilis repellendus ab minus, ex, deleniti voluptates possimus assumenda numquam delectus? Natus corporis architecto, consequatur quos maxime, alias dolore illo neque optio quo atque nulla laborum aliquid? Ipsum non dolores aliquam ratione molestiae fugiat magni voluptates! Ab possimus a, fuga quam hic excepturi!</p><br>
+                    <h3>Someone Famous</h3>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- contact -->
+    <section class="contact">
+        <h2 class="heading">Contact Us</h2>
+        <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur iste esse accusantium dolor error quas ipsa ullam adipisci, mollitia et qui eos. Provident deserunt nostrum explicabo reiciendis debitis voluptatibus sunt eos laudantium cumque dicta excepturi, quod architecto aperiam ad libero ex labore error enim nesciunt, assumenda eligendi mollitia asperiores laboriosam temporibus. Non molestiae numquam excepturi ut odio. Sunt vitae ea molestias ullam maxime accusantium consectetur quos sint voluptates dignissimos.</p>
+    </section>
+
+    <section class="about">
+        <div class="contentBx redbg">
+            <div class="form">
+                <div class="inputBx">
+                    <input type="text" name="" placeholder="Full Name">
+                </div>
+                <div class="inputBx">
+                    <input type="text" name="" placeholder="Email Address">
+                </div>
+                <div class="inputBx">
+                    <input type="text" name="" placeholder="Mobile no">
+                </div>
+                <div class="inputBx">
+                    <textarea name="" placeholder="write your message"></textarea>
+                </div>
+                <div class="inputBx">
+                    <input type="submit" name="" value="Send">
+                </div>
+            </div>
+        </div>
+        <div class="imgBx2"></div>
+    </section>
+    <!-- footer -->
+    <section class="footer">
+        <p class="text">Design & Developed By Online Tutorials</p>
+        <ul>
+            <p class="text">Follow us on:</p>
+            <li><a href=""><img src="assets/facebook.png" alt=""></a></li>
+            <li><a href=""><img src="assets/twitter.png" alt=""></a></li>
+            <li><a href=""><img src="assets/linkedin.png" alt=""></a></li>
+        </ul>
+    </section>
+</body>
+
+</html>
